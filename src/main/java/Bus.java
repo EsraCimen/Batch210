5f9fc2aa8d653068477fb159e066f54d97f24281
Esra Cimen <sifautopyasi@gmail.com>
2023-12-02 14:50:58 +0300

    first commit

---
+++ src/main/java/Bus.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bus {
+    //2-plaka ve koltuk no
+    public String numberPlate;//plaka
+    public List<String>seats =new ArrayList<>();
+
+    public Bus(String plaka){
+        this.numberPlate=plaka;
+        for (int i = 1; i <=32; i++) {
+            this.seats.add(String.valueOf(i));
+        }
+    }
+}
