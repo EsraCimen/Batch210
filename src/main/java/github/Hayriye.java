8dc837a23f2872355959e6ee2eb18df3d75be1e5
sevilayuyanik <sevilayturanuyanik@gmail.com>
2023-12-09 16:29:11 +0300

    lastversion

--- src/main/java/github/Hayriye.java
+++ src/main/java/github/Hayriye.java
@@ -3,5 +3,7 @@ package github;
 public class Hayriye {
     public static void main(String[] args) {
         System.out.println("Hello batch");
+
+        System.out.println("bakalım ne olacak");
     }
 }
