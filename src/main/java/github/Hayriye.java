e03fdba0fad9b46df250c029cb5221577a424402
seymabilgin <seymabilgin911@gmail.com>
2023-12-09 16:29:21 +0000

    v5

--- src/main/java/github/Hayriye.java
+++ src/main/java/github/Hayriye.java
@@ -3,5 +3,8 @@ package github;
 public class Hayriye {
     public static void main(String[] args) {
         System.out.println("Hello batch");
+
+        System.out.println(".............");
+
     }
 }
